3d58c96fb8ce912f1fb14a9182f4e8678105dc06
RamirezJosue <xdemobtkx@gmail.com>
2021-09-21 23:47:17 -0500

    iniciando cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'whatclient';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  sendMessage() {
+    const data = {
+      "codigo": "13074332",
+      "nombre": "Arcata Maquera Gumercindo",
+      "supervisor": "José Luis Rubin",
+      "bdr": "PEX124",
+      "riesgo": "alto",
+      "rechazo": "alto",
+      "rmd": "alto",
+      "cajas": "20.00",
+      "ruta": "BK7701",
+      "motivo": "no hay",
+      "localidad": "ILAVI",
+      "number": "51951291999"
+    }
+    this.http.post('http://localhost:3000/send', data).subscribe(resp => {
+      console.log(resp)
+    });
+  }
 }
